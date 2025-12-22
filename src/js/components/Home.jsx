463f150615425611c218923e39a320c20a738003
Jaime Vega <jaime.vegah@outlook.com>
2025-12-22 04:50:30 +0000

    se agregan los estilos, el componente Task y se termina casi en un 100 la logica de la tarea

--- src/js/components/Home.jsx
+++ src/js/components/Home.jsx
@@ -1,26 +1,43 @@
-import React from "react";
+import { useState } from 'react';
+import Task from './Task';
 
-//include images into your bundle
-import rigoImage from "../../img/rigo-baby.jpg";
-
-//create your first component
 const Home = () => {
-	return (
-		<div className="text-center">
-            
+	const [list, setList] = useState([{id: 1, task: 'Wash my hands'},{id: 2, task: 'Wash my card'}]);
+	const [ inputValue, setInputValue ] = useState('');
+	const deleteItem = (id) => {
+		console.log(id);
+		let newList = list; 
+		newList = newList.filter((task)=> task.id !== id); 
+		setList(newList);
+		
+	}
+	const ToDoList = list.map((item)=> <Task title={item.task} onClick ={deleteItem} key={item.id} id={item.id}/>)
 
-			<h1 className="text-center mt-5">Hello Rigo!</h1>
-			<p>
-				<img src={rigoImage} />
-			</p>
-			<a href="#" className="btn btn-success">
-				If you see this green button... bootstrap is working...
-			</a>
-			<p>
-				Made by{" "}
-				<a href="http://www.4geeksacademy.com">4Geeks Academy</a>, with
-				love!
-			</p>
+
+	return (
+		<div className="container-list">
+			<h1>TO DO LIST</h1>
+			<div className="list">
+				<form onSubmit={(e) => {
+						e.preventDefault();
+						let newList = list;
+						let position = list.length;
+						newList[position] = {id: position+1, task: inputValue}
+						console.log(newList);
+						setList(newList);
+						setInputValue('')
+					}}>
+					<input type="text" placeholder='What needs to be done?' 
+					onChange={(e)=> {
+						console.log(e.target.value);
+						setInputValue(e.target.value)
+						}}
+					value={inputValue}
+					/>
+				</form>
+				{ToDoList}
+				<span className='text-footer-list'>{`${list.length} item left`}</span>
+			</div>
 		</div>
 	);
 };
